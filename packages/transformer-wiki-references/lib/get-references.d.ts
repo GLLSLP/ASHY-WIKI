ed49b56c0fe67cf1b09442fbacdab93fee8d8b98
GLLSLP <woori@localhost.localdomain>
2023-03-02 17:46:01 +0900

    테스트중

---
+++ packages/transformer-wiki-references/lib/get-references.d.ts
@@ -0,0 +1,17 @@
+import { MdxNode } from './type';
+export declare type References = {
+    pages: Reference[];
+};
+export declare type Reference = {
+    /** target page slug */
+    target: string;
+    targetAnchor?: string;
+    contextLine: string;
+    referrerNode?: MdxNode;
+    label?: string;
+};
+export declare function rxWikiLink(): RegExp;
+export declare function rxBlockLink(): RegExp;
+export declare function rxHashtagLink(): RegExp;
+export declare function rxMarkdownLink(): RegExp;
+export declare const getReferences: (string: string, onReferenceAdd?: (ref: Reference) => Reference) => References;
