a553fd0e8b0cc9249f2e11f4ec9153013eb78633
GLLSLP <woori@localhost.localdomain>
2023-03-02 17:09:13 +0900

    test

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-// const { defaults: tsjPreset } = require('ts-jest/presets')
-
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jest-environment-jsdom-sixteen',
-  testRegex: `.*/__tests__/.*|\\.(spec)\\.tsx?$`,
-};
